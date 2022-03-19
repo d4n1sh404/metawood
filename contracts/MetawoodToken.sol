7cc206433092b7a7fbd3ac2a256d4b504a5d9737
Danish Siraj <danish@emertech.io>
2022-03-19 19:19:09 +0530

    Add: erc20 token for native token

---
+++ contracts/MetawoodToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MetawoodToken is ERC20 {
+
+    uint256 MAX_SUPPLY = 1000000 * 10**18;
+
+    constructor() ERC20("Metawood Token", "MTWD") {
+        _mint(msg.sender, MAX_SUPPLY );
+        approve(address(this), MAX_SUPPLY);
+    }
+
+    function requestToken(address requestor, uint256 amount) external {
+        transfer(requestor, amount);
+    }
+}
