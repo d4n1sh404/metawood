714de3166dfcb5e351ee9d22ac56bcee2dd525fe
Danish Siraj <danish@emertech.io>
2022-03-22 23:42:22 +0530

    Fix: faucet to transfer tokens to anyone

--- contracts/MetawoodToken.sol
+++ contracts/MetawoodToken.sol
@@ -2,17 +2,21 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MetawoodToken is ERC20 {
-
-    uint256 public constant MAX_SUPPLY  = 1000000 * 10**18;
+contract MetawoodToken is ERC20, Ownable {
+    uint256 public constant MAX_SUPPLY = 1000000 * 10**18;
+    IERC20 private _self;
 
     constructor() ERC20("Metawood Token", "MTWD") {
-        _mint(msg.sender, MAX_SUPPLY );
+        _mint(msg.sender, MAX_SUPPLY);
         approve(address(this), MAX_SUPPLY);
+        _self = IERC20(address(this));
     }
 
     function requestToken(address requestor, uint256 amount) external {
-        transfer(requestor, amount);
+        approve(msg.sender, amount);
+        _self.transferFrom(owner(), requestor, amount);
     }
 }
