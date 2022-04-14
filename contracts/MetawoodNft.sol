593335613b3b92ec2a5e2072c6c8751e7e2545a8
Danish Siraj <danish@emertech.io>
2022-04-14 18:17:13 +0530

    Update: nft minted event to have the token uri as well

--- contracts/MetawoodNft.sol
+++ contracts/MetawoodNft.sol
@@ -31,7 +31,7 @@ contract MetawoodNFT is
 
     event SetTokenURI(uint256 tokenId, string tokenURI);
     event SetBaseURI(string metadataBaseURI);
-    event MetawoodNFTMinted(uint256 tokenId, address creator);
+    event MetawoodNFTMinted(uint256 tokenId, string tokenURI, address creator);
 
     modifier ensureNonZeroAddress(address addressToCheck) {
         require(addressToCheck != address(0), "Metawood NFT: No zero address");
@@ -87,7 +87,7 @@ contract MetawoodNFT is
         require(_amount >= 1, "MetawoodNFT: invalid amount parameter");
         _mint(_account, _tokenIdCounter.current(), _amount, _data);
         _uris[_tokenIdCounter.current()] = _tokenUrl;
-        emit MetawoodNFTMinted(_tokenIdCounter.current(), _account);
+        emit MetawoodNFTMinted(_tokenIdCounter.current(),_tokenUrl, _account);
         _tokenIdCounter.increment();
     }
 
@@ -108,7 +108,7 @@ contract MetawoodNFT is
             require(_amounts[i] >= 1, "MetawoodNFT: invalid amount parameter");
             _ids[i] = _tokenIdCounter.current();
             _uris[_tokenIdCounter.current()] = _tokenUrls[i];
-            emit MetawoodNFTMinted(_tokenIdCounter.current(), _account);
+            emit MetawoodNFTMinted(_tokenIdCounter.current(),_tokenUrls[i], _account);
             _tokenIdCounter.increment();
         }
         _mintBatch(_account, _ids, _amounts, _data);
