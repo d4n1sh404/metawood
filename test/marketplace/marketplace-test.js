7c3a8b324741f88e7f68fad04c87f07085a8cd31
Danish Siraj <danish@emertech.io>
2022-04-14 16:16:56 +0530

    Add: basic functionality tests

--- test/marketplace/marketplace-test.js
+++ test/marketplace/marketplace-test.js
@@ -1,10 +1,10 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const { BigNumber } = require("ethers");
+const { keccak256 } = require("ethers/lib/utils");
 
 let marketPlace;
 let nftContract;
-let nativeToken;
 let deployer;
 let user;
 let userTwo;
@@ -15,121 +15,94 @@ describe("Metawood Marketplace", () => {
     user = (await ethers.getSigners())[1];
     userTwo = (await ethers.getSigners())[2];
 
-    const NftContract = await ethers.getContractFactory("MetawoodNft");
-    nftContract = await NftContract.deploy();
+    const NftContract = await ethers.getContractFactory("MetawoodNFT");
+    nftContract = await NftContract.deploy("");
 
     await nftContract.deployed();
 
-    const TokenContract = await ethers.getContractFactory("MetawoodToken");
-    nativeToken = await TokenContract.deploy();
-
-    const MarketPlaceContract = await ethers.getContractFactory("MetawoodMarketPlace");
+    const MarketPlaceContract = await ethers.getContractFactory("MetawoodNFTMarketPlaceV1");
     marketPlace = await MarketPlaceContract.deploy(nftContract.address);
 
-    await nativeToken.deployed();
     await marketPlace.deployed();
   });
 
-  it("Faucet should be active!", async function () {
-    let tx = await nativeToken
-      .connect(user)
-      .requestToken(user.address, BigNumber.from("100000000"));
 
-    let u2Tx = await nativeToken
-      .connect(userTwo)
-      .requestToken(userTwo.address, BigNumber.from("100000000"));
-
-    let tx2 = await nativeToken.connect(deployer).balanceOf(user.address);
-    expect(BigNumber.from(tx2)).to.be.equal(BigNumber.from("100000000"));
+  it("Should register marketplace on nftToken Contract!", async function () {
+    let tx = await nftContract.setMetawoodMarketPlace(marketPlace.address);
   });
 
-  it("Should register native Token!", async function () {
-    let tx = await marketPlace.addSupportedToken("native", nativeToken.address);
-  });
-
-  it("Should register marketplace on nftToken!", async function () {
-    let tx = await nftContract.setMarketPlace(marketPlace.address);
+  it("Should grant minter role!", async function () {
+    let role = keccak256(ethers.utils.toUtf8Bytes("MINTER_ROLE"));
+    let tx = await nftContract.grantRole(role, user.address);
   });
 
   it("Should mint a new token!", async function () {
-    let tx = await nftContract.connect(user).mint(1, "http://testing", "0x00");
+    let tx = await nftContract.connect(user).mint(user.address, 1, "http://testing", "0x00");
   });
 
   it("Minted token should have custom uri", async function () {
-    let tx = await nftContract.uri(1);
+    let tx = await nftContract.uri(0);
     expect(tx).to.equal("http://testing");
   });
 
+
   it("User should have minted token!", async function () {
-    let tx = await nftContract.balanceOf(user.address, 1);
+    let tx = await nftContract.balanceOf(user.address, 0);
     expect(BigNumber.from(tx)).to.be.equal(1);
   });
 
+
   it("User should be able to list minted token!", async function () {
-    let tx = await marketPlace.connect(user).createListing(1, 100);
+    let tx = await marketPlace.connect(user).createListing(0, 1);
   });
 
+
   it("Listing should have been created!", async function () {
-    let tx = await marketPlace.getListing(1);
-    expect(BigNumber.from(tx[0])).to.be.equal(1); //listingId
-    expect(tx[1]).to.be.equal(user.address); //creatorAddress
-    expect(BigNumber.from(tx[2])).to.be.equal(1); //tokenId
-    expect(BigNumber.from(tx[3])).to.be.equal(100); //lisingPrice
+    let tx = await marketPlace.getListing(0);
+    expect(tx.listingId).to.be.equal(0); //listingId
+    expect(tx.seller).to.be.equal(user.address); //creatorAddress
+    expect(tx.tokenId).to.be.equal(0); //tokenId
+    expect(tx.tokenPrice).to.be.equal(1); //lisingPrice
   });
+
 
   it("Should get latest listings!", async function () {
     let tx = await marketPlace.getLatestListings(3);
     expect(tx.length).to.be.equal(3);
   });
 
+
   it("User Should get his open listings!", async function () {
     let tx = await marketPlace.connect(user).getOpenListings(user.address);
     expect(tx.length).to.be.equal(1);
   });
+
 
   it("Should get all open listings!", async function () {
     let tx = await marketPlace.getAllOpenListings();
     expect(tx.length).to.be.equal(1);
   });
 
+
   it("Should list owned tokens!", async function () {
     let tx = await marketPlace.connect(user).getOwnedTokens(user.address);
-    expect(BigNumber.from(tx[0])).to.be.equal(1); //tokenId
+    expect(BigNumber.from(tx[0])).to.be.equal(0); //tokenId
   });
 
   it("Listing should be bought!", async function () {
-    // console.log(await marketPlace.isApprovedForAll(deployer.address,marketPlace.address));
-    let approve = await nativeToken
-      .connect(userTwo)
-      .approve(marketPlace.address, ethers.constants.MaxUint256);
-
-    let tx = await marketPlace.connect(userTwo).buyNft(1);
-    let tx2 = await nftContract.balanceOf(userTwo.address, 1);
-    expect(BigNumber.from(tx2)).to.be.equal(1);
+    let tx = await marketPlace.connect(userTwo).purchaseNFT(0,{value: 1});
+    let tx2 = await nftContract.balanceOf(userTwo.address, 0);
+    expect(BigNumber.from(tx2)).to.be.equal(1); 
   });
 
+
   it("Buyer should be able to list bought token!", async function () {
-    let tx = await marketPlace.connect(userTwo).createListing(1, 200);
+    let tx = await marketPlace.connect(userTwo).createListing(0, 2);
   });
 
   it("User should be able to close  his listing!", async function () {
-    let tx = await marketPlace.connect(userTwo).closeListing(2);
+    let tx = await marketPlace.connect(userTwo).closeListing(1);
   });
 
-  it("Should register a new User!", async function () {
-    let tx = await marketPlace.connect(user).saveUser( "datauri");
-  });
-
-  it("Should get the registered User!", async function () {
-    let tx = await marketPlace.getUser(user.address);
-    expect(tx.data == "datauri");
-  });
-
-
-  it("Should update the User!", async function () {
-    await marketPlace.connect(user).saveUser("newdata");
-    let tx = await marketPlace.getUser(user.address);
-    expect(tx.data == "newdata");
-  });
 
 });
