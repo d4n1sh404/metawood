bffb4f62ab0c65f1bb474515cff6f11ecf67f5fc
Saurabh Lodha <saurabhlodha221b@gmail.com>
2022-04-18 20:57:00 +0530

    Changed contract methods to external

--- contracts/MetawoodNFTMarketPlaceV1.sol
+++ contracts/MetawoodNFTMarketPlaceV1.sol
@@ -189,11 +189,11 @@ contract MetawoodNFTMarketPlaceV1 is Ownable, ReentrancyGuard, Pausable {
         TransferHelper.safeTransferETH(msg.sender, address(this).balance);
     }
 
-    function pause() public onlyOwner {
+    function pause() external onlyOwner {
         _pause();
     }
 
-    function unpause() public onlyOwner {
+    function unpause() external onlyOwner {
         _unpause();
     }
 
